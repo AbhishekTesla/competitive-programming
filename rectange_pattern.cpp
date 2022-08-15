79b103a8d6b9ff27ce5cd1f73e5d3ec1be0f2c8a
Abhishek <jangal&mangal.gov>
2022-08-16 03:12:52 +0530

    Day 1

---
+++ rectange_pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,row,col;
+
+    cout<<"Enter the number of rows and column"<<endl;
+    cin>>row>>col;
+
+    for(i=1;i<=row;i++){
+        for(j=1;j<=col;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
