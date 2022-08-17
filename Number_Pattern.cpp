a3b21b2f40d7e6b172820e9eda9daa4e80d730cc
Abhishek <jangal&mangal.gov>
2022-08-17 19:58:43 +0530

    Day 2

---
+++ Number_Pattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+    int i, j, space, n;
+
+    cout << "Enter the value of n : " << endl;
+
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (space = 1; space <= n - i; space++)
+        {
+            cout << " ";
+        }
+
+        for (j = 1; j <= i; j++)
+        {
+            cout << j
+                 << " ";
+        }
+
+        cout << endl;
+    }
+}
