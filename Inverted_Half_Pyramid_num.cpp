2ad68f422624d1bbe57930bdc253b15e0f33450e
Abhishek <jangal&mangal.gov>
2022-08-17 19:39:52 +0530

    Day 2

---
+++ Inverted_Half_Pyramid_num.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j, row;
+
+    cout << "Enter the number of rows " << endl;
+    cin >> row;
+
+    for (i = row; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+
+            cout<<j<<" ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
